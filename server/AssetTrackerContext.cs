c1b2c10078f1bd6a5cf4b99c81db9751ee6f46be
unknown <david.n.johansson@live.com>
2021-02-17 17:43:57 +0100

    ..

---
+++ server/AssetTrackerContext.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace server
+{
+    public class AssetTrackerContext : DbContext
+    {
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<FormFactor> FormFactors { get; set; }
+        public DbSet<Asset> Assets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=.\SQLEXPRESS;Database=AssetTracker;Trusted_Connection=True;");
+    }
+
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double ExchangeRateRelativeToDollar { get; set; }
+        public List<Office> Offices { get; } = new List<Office>();
+    }
+
+    public class Office
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public int CurrencyId { get; set; }
+        public Currency Currency { get; set; }
+        public List<Asset> Assets { get; } = new List<Asset>();
+    }
+
+    public class FormFactor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Asset> Assets { get; } = new List<Asset>();
+    }
+
+    public class Asset
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double PurchasePrice { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int OfficeId { get; set; }
+        public Office Office { get; set; }
+        public int FormFactorId { get; set; }
+        public FormFactor FormFactor { get; set; }
+    }
+}
---
+++ server/Migrations/20210217164017_InitialCreate.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExchangeRateRelativeToDollar = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormFactors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormFactors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Offices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrencyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Offices_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Assets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PurchasePrice = table.Column<double>(type: "float", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OfficeId = table.Column<int>(type: "int", nullable: false),
+                    FormFactorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Assets_FormFactors_FormFactorId",
+                        column: x => x.FormFactorId,
+                        principalTable: "FormFactors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Assets_Offices_OfficeId",
+                        column: x => x.OfficeId,
+                        principalTable: "Offices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assets_FormFactorId",
+                table: "Assets",
+                column: "FormFactorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assets_OfficeId",
+                table: "Assets",
+                column: "OfficeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offices_CurrencyId",
+                table: "Offices",
+                column: "CurrencyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Assets");
+
+            migrationBuilder.DropTable(
+                name: "FormFactors");
+
+            migrationBuilder.DropTable(
+                name: "Offices");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+        }
+    }
+}
