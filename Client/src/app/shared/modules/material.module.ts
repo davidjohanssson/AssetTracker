82f83f95820123cc339745e46849030f8c6f5eeb
unknown <david.n.johansson@live.com>
2021-02-24 16:44:56 +0100

    ..

--- Client/src/app/shared/modules/material.module.ts
+++ Client/src/app/shared/modules/material.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
     imports: [
         MatButtonModule,
         MatInputModule,
         MatSelectModule,
+        MatIconModule,
+        MatMenuModule,
     ],
     exports: [
         MatButtonModule,
         MatInputModule,
         MatSelectModule,
+        MatIconModule,
+        MatMenuModule,
     ]
 })
 export class MaterialModule { }
